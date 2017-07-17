3462d8211ccdf8bd2c36f1fe2a5a63a6fef659d5
Michael Francoeur <mjfrancoeur@gmail.com>
2017-07-17 10:08:45 -0400

    Add successful GET request to USDA API
    Use input form to search for food products, returns array of food items
    with sugar content.

--- app.js
+++ app.js
@@ -1,33 +1,57 @@
 function App() {
 
-  // Ajax request
-  // $.get('https://api.nal.usda.gov/ndb/V2/reports?', {
-  //   ndbno: "01009",
-  //   type: 'b',
-  //   format: 'json',
-  //   api_key: 'XM57A3PgIUrZfDaDxBgSB0Fba56m8jPUO5vbYT5w',
-  // }).done(function(data) {
-  //   console.log(data);
-  // })
-  // 
-   // search request
-   $.get('https://api.nal.usda.gov/ndb/search/?', {
-     api_key: 'XM57A3PgIUrZfDaDxBgSB0Fba56m8jPUO5vbYT5w',
-     q: 'wheaties',
-     format: 'json'
-   }).done(function(data) {
-     console.log('search '+  data);
-   })
+  const $searchBar = $('.search-bar');
+  const FOODS = [];
 
-  // nutrient request
-  // $.get('https://api.nal.usda.gov/ndb/nutrients/?', {
-  //   api_key: 'XM57A3PgIUrZfDaDxBgSB0Fba56m8jPUO5vbYT5w',
-  //   ndbno: '01009',
-  //   nutrients: '269',
-  //   format: 'json'
-  // }).done(function(data) {
-  //   console.log('search ' +  data);
-  // })
+  function Food(params) {
+    this.name = params.name;
+    this.ndbno = params.ndbno;
+    this.sugars = {
+      value: params.sugars.value,
+      unitOfMeasure: params.sugars.unitOfMeasure
+    };
+    this.servingSize = params.servingSize;
+    this.imgURL = params.imgURL || null;
+  }
+
+  $("#searchForm").on('submit', function(event) {
+    // prevent page from reloading
+    event.preventDefault();
+
+    clearPrevSearch();
+    // search request
+    $.get('https://api.nal.usda.gov/ndb/search/?', $("#searchForm").serialize())
+      .done(function(data) {
+        data.list.item.forEach( (el) => {
+          nutrientRequest(el.ndbno);
+        });
+        console.dir(FOODS);
+      })
+  });
+
+  function clearPrevSearch() {
+    FOODS.splice(0);
+  }
+
+  function nutrientRequest(foodID) {
+    $.get('https://api.nal.usda.gov/ndb/nutrients/?', {
+      api_key: 'XM57A3PgIUrZfDaDxBgSB0Fba56m8jPUO5vbYT5w',
+      ndbno: foodID,
+      nutrients: '269',
+      format: 'json'
+    }).done(function(data) {
+      FOODS.push(new Food({
+        name: data.report.foods[0].name,
+        ndbno: data.report.foods[0].ndbno,
+        sugars: {
+          value: data.report.foods[0].nutrients[0].value,
+          unitOfMeasure: data.report.foods[0].nutrients[0].unit
+        },
+        servingSize: data.report.foods[0].measure
+      }));
+    })
+  }
+
 }
 
 $(App);
