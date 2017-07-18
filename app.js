5b0448597f83cf1e002a00ad9fc167c99cedbda0
Michael Francoeur <mjfrancoeur@gmail.com>
2017-07-18 11:50:09 -0400

    Fix linting issues

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 function App() {
-
-  const $searchBar = $('.search-bar');
   const FOODS = [];
 
   function Food(params) {
@@ -8,51 +6,11 @@ function App() {
     this.ndbno = params.ndbno;
     this.sugars = {
       value: params.sugars.value,
-      unitOfMeasure: params.sugars.unitOfMeasure
+      unitOfMeasure: params.sugars.unitOfMeasure,
     };
     this.servingSize = params.servingSize;
     this.imgURL = params.imgURL || null;
   }
-
-  $("#searchForm").on('submit', function(event) {
-
-    // prevent page from reloading
-    event.preventDefault();
-
-    // clear previous search
-    clearPrevSearch();
-
-    // search request
-    let $searchResults = 
-      $.get(
-        'https://api.nal.usda.gov/ndb/search/?', 
-        $("#searchForm").serialize()
-      )
-        .done(function(data) {
-
-          let results = data.list.item.map(function(result) {
-            return nutrientRequest(result.ndbno);
-          });
-
-          $.when.apply($, results).done(function () {
-            [].slice.call(arguments).forEach( (item) => {
-
-            FOODS.push(new Food({
-              name: item[0].report.foods[0].name,
-              ndbno: item[0].report.foods[0].ndbno,
-              sugars: {
-                value: item[0].report.foods[0].nutrients[0].value,
-                unitOfMeasure: item[0].report.foods[0].nutrients[0].unit
-              },
-              servingSize: item[0].report.foods[0].measure
-            }));
-            });
-
-            render();
-          });
-
-        });
-
 
   function clearPrevSearch() {
     FOODS.splice(0);
@@ -64,21 +22,19 @@ function App() {
       api_key: 'XM57A3PgIUrZfDaDxBgSB0Fba56m8jPUO5vbYT5w',
       ndbno: foodID,
       nutrients: '269',
-      format: 'json'
+      format: 'json',
     });
   }
 
   function render() {
-    FOODS.forEach( (food) => {
+    FOODS.forEach((food) => {
       const $wrapper = $('<div>').addClass('result-wrapper');
       const $img = $('<img>').addClass('result-col food-img')
         .attr('src', 'http://www.cheerios.com/~/media/17EE88F6F39C45E787CE2E1186260B94.ashx').appendTo($wrapper);
 
       const $textbox = $('<div>').addClass('result-col result-textbox').appendTo($wrapper);
       const $header = $('<h3>').text(food.name).appendTo($textbox);
-      const $para = $('<h4>').text(food.sugars.value + food.sugars.unitOfMeasure + ' of sugar per serving').appendTo($textbox);
-
-
+      const $para = $('<h4>').text(`${food.sugars.value}${food.sugars.unitOfMeasure} of sugar per serving`).appendTo($textbox);
 
       const $graph = $('<div>').addClass('result-col').appendTo($wrapper);
 
@@ -86,6 +42,41 @@ function App() {
     });
   }
 
+
+  $('#searchForm').on('submit', (event) => {
+    // prevent page from reloading
+    event.preventDefault();
+
+    // clear previous search
+    clearPrevSearch();
+
+    // search request
+    const $searchResults =
+      $.get(
+        'https://api.nal.usda.gov/ndb/search/?',
+        $('#searchForm').serialize(),
+      )
+      .done((data) => {
+        const results = data.list.item.map((result) => {
+          return nutrientRequest(result.ndbno);
+        });
+
+        $.when.apply($, results).done(() => {
+          [].slice.call(arguments).forEach((item) => {
+            FOODS.push(new Food({
+              name: item[0].report.foods[0].name,
+              ndbno: item[0].report.foods[0].ndbno,
+              sugars: {
+                value: item[0].report.foods[0].nutrients[0].value,
+                unitOfMeasure: item[0].report.foods[0].nutrients[0].unit,
+              },
+              servingSize: item[0].report.foods[0].measure,
+            }));
+          });
+
+          render();
+        });
+      });
   });
 }
 
